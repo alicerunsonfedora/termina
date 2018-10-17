f4886fc42bdbe5f626a820941e51a2561cbb0090
Marquis Kurt <software@marquiskurt.net>
2018-10-17 17:27:38 -0400

    Initial Commit

---
+++ SwiftMUD/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftMUD
+//
+//  Created by Marquis Kurt on 10/17/18.
+//  Copyright © 2018 Marquis Kurt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
